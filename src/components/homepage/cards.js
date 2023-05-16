cc691ce8ba1420bae3c776f40c2dee4ddaf66faf
Mark Stephen <mark.zizon@gmail.com>
2023-05-16 19:57:48 +0800

    [bug/route] bug fix on card routes

--- src/components/homepage/cards.js
+++ src/components/homepage/cards.js
@@ -6,7 +6,7 @@ const Cards = () => {
         <>
             {
                 topics.map((card, index) =>
-                    <Link className="topics-card" key={index} to={`/topic${card.path}`} role="link" aria-label={`Navigate to ${card.label} topic`}>
+                    <Link className="topics-card" key={index} to={`/unpuzzle-help-centre/topic${card.path}`} role="link" aria-label={`Navigate to ${card.label} topic`}>
                         <img src={process.env.PUBLIC_URL + '/images/topics/' + card.icon} alt="" />
                         <h4>{card.label}</h4>
                     </Link>
